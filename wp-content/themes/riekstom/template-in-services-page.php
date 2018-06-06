5d4f76a78ea58fe0e11611e5e8eafaa416a39827
Середа Дмитрий <q3dm0@yahoo.com>
2018-06-06 17:31:11 +0300

    Переделан блок услуг на главной странице

---
+++ wp-content/themes/riekstom/template-in-services-page.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template name: In services page
+Theme Name: Riekstom
+Theme URI: http://riekstom.com/
+Author: Thebarber
+Author URI: http://mkvadrat.com/
+Description: Тема для сайта http://riekstom.com/
+Version: 1.0
+*/
+
+get_header(); 
+?>
+
+    
+  
+<?php get_footer(); ?>
